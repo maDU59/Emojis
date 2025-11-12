f09b6186914ae7cf986b02efc8666e0840cb42e8
maDU59_ <matthias.ducourant@gmail.com>
2025-11-12 18:14:06 +0100

    Started the mixin

--- src/client/java/fr/madu59/mixin/client/ExampleClientMixin.java
+++ src/client/java/fr/madu59/mixin/client/ExampleClientMixin.java
@@ -1,15 +1,51 @@
 package fr.madu59.mixin.client;
 
 import net.minecraft.client.Minecraft;
+
+import java.util.Collection;
+import java.util.regex.Pattern;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Minecraft.class)
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+
+import net.minecraft.client.gui.components.CommandSuggestions;
+import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.commands.SharedSuggestionProvider;
+
+@Mixin(CommandSuggestions.class)
 public class ExampleClientMixin {
-	@Inject(at = @At("HEAD"), method = "run")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of Minecraft.run()V
+
+	private static final Pattern WHITESPACE_PATTERN = Pattern.compile("(\\s+)");
+
+	@Shadow
+    @Final
+    EditBox input;
+
+	@Shadow
+    @Final
+    boolean commandsOnly;
+	
+	@Inject(at = @At("TAIL"), method = "updateCommandInfo")
+	private void updateCommandInfo(CallbackInfo info) {
+		String string = this.input.getValue();
+		StringReader stringReader = new StringReader(string);
+		boolean startsWithSlash = stringReader.canRead() && stringReader.peek() == '/';
+		if (startsWithSlash) {
+			stringReader.skip();
+		}
+
+		boolean isCommand = this.commandsOnly || startsWithSlash;
+		int cursorPos = this.input.getCursorPosition();
+		if (!isCommand) {
+			String beforeCursorText = string.substring(0, cursorPos);
+		}
 	}
 }
