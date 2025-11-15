9269c1893cd98852059abd63f5cc491b41954bb8
maDU59_ <matthias.ducourant@gmail.com>
2025-11-15 13:38:12 +0100

    Added some more emojis as well as a mod icon

--- src/client/java/fr/madu59/emoji/EmojiCommand.java
+++ src/client/java/fr/madu59/emoji/EmojiCommand.java
@@ -18,10 +18,10 @@ public class EmojiCommand {
             dispatcher.register(
                 literal("emojis")
                 .then(literal("add")
-                    .then(argument("id", StringArgumentType.word())
+                    .then(argument("slug", StringArgumentType.word())
                         .then(argument("value", StringArgumentType.greedyString())
                             .executes(context -> {
-                                boolean success = CustomEmojiManager.addEmoji(StringArgumentType.getString(context, "id"), StringArgumentType.getString(context, "value"));
+                                boolean success = CustomEmojiManager.addEmoji(StringArgumentType.getString(context, "slug"), StringArgumentType.getString(context, "value"));
                                 if(success) feedbackMessage(Component.translatable("emoji-added-success"));
                                 else feedbackMessage(Component.translatable("emoji-added-fail"));
                                 return 1;
@@ -36,9 +36,9 @@ public class EmojiCommand {
             dispatcher.register(
                 literal("emojis")
                 .then(literal("remove")
-                    .then(argument("id", StringArgumentType.word())
+                    .then(argument("slug", StringArgumentType.word())
                         .executes(context -> {
-                            boolean success = CustomEmojiManager.removeEmoji(StringArgumentType.getString(context, "id"));
+                            boolean success = CustomEmojiManager.removeEmoji(StringArgumentType.getString(context, "slug"));
                             if(success) feedbackMessage(Component.translatable("emoji-removed-success"));
                             else feedbackMessage(Component.translatable("emoji-removed-fail"));
                             return 1;
@@ -54,6 +54,7 @@ public class EmojiCommand {
                 .then(literal("customlist")
                     .executes(context -> {
                         if(CustomEmojiManager.customEmojiMap.size() > 0){
+                            feedbackMessage(Component.translatable("display-custom-emoji-list"));
                             for (Map.Entry<String, String> entry : CustomEmojiManager.customEmojiMap.entrySet()){
                                 feedbackMessage(Component.literal(entry.getKey() + " " + entry.getValue()));
                             }
