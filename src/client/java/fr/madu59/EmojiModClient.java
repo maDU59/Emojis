bd275a48ed9057f0190ff1be1f35450f452ee309
maDU59_ <matthias.ducourant@gmail.com>
2025-11-15 15:39:50 +0100

    Some refactoring

--- src/client/java/fr/madu59/EmojiModClient.java
+++ src/client/java/fr/madu59/EmojiModClient.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.server.packs.PackType;
 
-public class ExampleModClient implements ClientModInitializer {
+public class EmojiModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
